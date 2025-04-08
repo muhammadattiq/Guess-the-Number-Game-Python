b1151cdfa37c6b4c94b6aad03c1e0322be5bcd74
Muhammad Attiq <muhammadattiq7@gmail.com>
2025-04-09 00:01:37 +0430

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,27 @@
+## In this prgram a rndon number will be generated and user has to guess the random generated number ##
+
+import random
+
+def guess():
+    a = random.randint(0,10)
+    while True:
+        user = int(input("Enter a number"))
+        if(user>a):
+            print("your guess is too high")
+            print(a)
+        elif(user<a):
+            print("your guess is too low")
+            print(a)
+        else:
+            print("You won!!")
+            prompt = input("Do you wanna play more? if yes then (Y) if no then (N)")
+            if(prompt=="Y"):
+                guess()  
+            elif(prompt=="N"):
+                print("Dafa Hooo!!")   
+            break       
+            
+guess()
+
+
+
